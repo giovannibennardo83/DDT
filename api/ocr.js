79faddb2601bc6a792acb5fb49bc7030fc1a4059
giovannibennardo83 <giovannibennardo83@gmail.com>
2026-03-18 17:13:40 +0100

    Update ocr.js

--- api/ocr.js
+++ api/ocr.js
@@ -2,6 +2,15 @@ import OpenAI from "openai";
 
 export default async function handler(req, res) {
 
+  // CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   try {
 
     if (req.method !== "POST") {
