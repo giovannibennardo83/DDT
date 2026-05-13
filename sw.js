68eee8f3c1740e86f793f5b015284f1eaa4c1060
giovannibennardo83 <giovannibennardo83@gmail.com>
2026-05-13 18:54:09 +0200

    Revert "Implement conservative hybrid caching strategy in service worker"

--- sw.js
+++ sw.js
@@ -10,26 +10,6 @@ const ASSETS = [
   './manifest.json'
 ];
 
-const NETWORK_FIRST_PATHS = new Set([
-  '/',
-  './',
-  '/index.html',
-  './index.html',
-  '/app.js',
-  './app.js',
-  '/db.js',
-  './db.js'
-]);
-
-const CACHE_FIRST_PATHS = new Set([
-  '/styles.css',
-  './styles.css',
-  '/print.css',
-  './print.css',
-  '/manifest.json',
-  './manifest.json'
-]);
-
 self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
@@ -43,32 +23,5 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-
-  const url = new URL(event.request.url);
-  const path = url.pathname;
-
-  if (NETWORK_FIRST_PATHS.has(path)) {
-    console.log('[SW] network-first:', path);
-    event.respondWith(
-      fetch(event.request)
-        .then((response) => {
-          if (response && response.ok) {
-            const responseClone = response.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
-          }
-          return response;
-        })
-        .catch(() => {
-          console.log('[SW] cache fallback:', path);
-          return caches.match(event.request);
-        })
-    );
-    return;
-  }
-
-  if (CACHE_FIRST_PATHS.has(path)) {
-    console.log('[SW] cache-first:', path);
-    event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
-  }
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
 });
